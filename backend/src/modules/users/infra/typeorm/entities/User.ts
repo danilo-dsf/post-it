ba99af3d81dc17d6afa2d3bbea26b69cf3c1248f
Danilo Ferreira <danilo1705@gmail.com>
2021-02-07 16:15:00 -0300

    User model created

---
+++ backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -0,0 +1,53 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Exclude, Expose } from 'class-transformer';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  birthdate: Date;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Expose()
+  getAvatarUrl(): string | null {
+    if (!this.avatar) {
+      return null;
+    }
+
+    return `${process.env.APP_API_URL}/files/${this.avatar}`;
+  }
+}
+
+export default User;
